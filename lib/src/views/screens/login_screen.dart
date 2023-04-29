35ba223f5514a3bdfdbc319d49aaaf83e3e84597
zamzamnurahman <kazamnn1912@gmail.com>
2023-04-29 21:14:08 +0700

    add login screen

--- lib/src/views/screens/login_screen.dart
+++ lib/src/views/screens/login_screen.dart
@@ -1,11 +1,157 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class LoginScreen extends ConsumerWidget {
+import '../../core/style/theme.dart';
+import '../components/form_input.dart';
+
+class LoginScreen extends ConsumerStatefulWidget {
   const LoginScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return const Scaffold();
+  ConsumerState<ConsumerStatefulWidget> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends ConsumerState<LoginScreen> {
+  final TextEditingController usernameCtrl = TextEditingController();
+  final TextEditingController passCtrl = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  void handleLogin() {
+    if (_formKey.currentState!.validate()) {
+    } else {}
+  }
+
+  @override
+  void dispose() {
+    usernameCtrl.dispose();
+    passCtrl.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Stack(children: [
+        ListView(
+          children: [
+            Container(
+              height: size.height,
+              width: size.width,
+              decoration: const BoxDecoration(
+                  image: DecorationImage(
+                image: AssetImage(
+                  "assets/images/bg.png",
+                ),
+                fit: BoxFit.cover,
+              )),
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 15.0, sigmaY: 15.0),
+                child: Container(
+                  margin: const EdgeInsets.symmetric(
+                    horizontal: 40,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      buildHeader(),
+                      const SizedBox(height: 15),
+                      buildFormLogin()
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        )
+      ]),
+    );
+  }
+
+  Column buildHeader() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: const [
+        Text(
+          "E-learning",
+          style: TextStyle(fontSize: 34, fontWeight: FontWeight.w700),
+        ),
+        Text(
+          "Layanan Digitalisasi Sekolah",
+          style: TextStyle(fontSize: 16, color: Color(0xFF06283D)),
+        ),
+      ],
+    );
+  }
+
+  Card buildFormLogin() {
+    return Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+      elevation: 5,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          const SizedBox(height: 20),
+          const Text(
+            "Selamat Datang",
+            style: TextStyle(
+              fontFamily: "Poppins",
+              fontSize: 18,
+              color: Color(0xff256D85),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.all(20),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    TextForm(
+                      usernameCtrl,
+                      "Masukkan Nim",
+                      'Username',
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Harap Isi Username";
+                        }
+                        return null;
+                      },
+                    ),
+                    TextForm(
+                      passCtrl,
+                      'Masukkan Password',
+                      "Password",
+                      isObsecure: true,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Harap Isi Username";
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.all(15),
+                          backgroundColor: kGreenPrimary,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(
+                            10,
+                          ))),
+                      onPressed: handleLogin,
+                      child: const Text("Masuk"),
+                    )
+                  ]),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
